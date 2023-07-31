91902d1d633399e423e4044f1e8ba874669365aa
Chandan Rana <chandan316@outlook.com>
2023-07-31 16:06:04 +0530

    updated gitignore

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Function to toggle theme between light and dark
+function toggleTheme() {
+  const themeToggle = document.getElementById("theme-toggle");
+  themeToggle.classList.toggle("dark");
+}
